03fd89dc1049fc33d1a18b90b371176485d40771
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2023-02-09 19:35:08 +0400

    Add files via upload

---
+++ druked_beta_new/js/notif.js
@@ -0,0 +1,8 @@
+let paramString = urlString.split('?')[1];
+let queryString = new URLSearchParams(paramString);
+
+for (let pair of queryString.entries()) {
+   console.log("Key is: " + pair[0]);
+   console.log("Value is: " + pair[1]);
+}
+
