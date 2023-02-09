a0ad846c59dde46bcd6910d6e697f969934aba3f
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2023-02-09 23:57:42 +0400

    Delete druked_beta_new directory

--- druked_beta_new/js/notif.js
+++
@@ -1,8 +0,0 @@
-let paramString = urlString.split('?')[1];
-let queryString = new URLSearchParams(paramString);
-
-for (let pair of queryString.entries()) {
-   console.log("Key is: " + pair[0]);
-   console.log("Value is: " + pair[1]);
-}
-
